26b1f62481a2889a146d1cc712f5b53a82fd5ed4
JevinLeon <jevinleon@gmail.com>
2024-05-23 21:47:06 +0700

    installed bootstrap & react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomePage,
+  LoginPage,
+  NotFoundPage,
+  OTPPage,
+  RegisterPage,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/OTP",
+    element: <OTPPage />,
+  },
+  {
+    path: "/*",
+    element: <NotFoundPage />,
+  },
+]);
+
 const App = () => {
-  return <div>Hello Travel Sky!</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
