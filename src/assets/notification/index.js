d2dee92189fff13746ebc4ef0903e08d5708882a
aisyahwlandr <aisyahwlandr@gmail.com>
2024-05-26 11:02:03 +0700

    notification: third layout

---
+++ src/assets/notification/index.js
@@ -0,0 +1,12 @@
+// assets/notification/index.js
+import arrowLeft from './arrow-left.svg';
+import filterButton from './filter.svg';
+import searchButton from './search.svg';
+import notifButton from './notif.svg';
+
+export {
+    arrowLeft,
+    filterButton,
+    searchButton,
+    notifButton,
+};
--- src/pages/notification/index.jsx
+++ src/pages/notification/index.jsx
@@ -1,33 +1,50 @@
 import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, Image } from "react-bootstrap";
 import "./notification.css";
+import * as icons from "../../assets/notification";
 
 const NotificationPage = () => {
     return (
         <>
             <div className="shadow">
-                <Container className="py-3">
+                <Container className="py-4">
                     <h4 className="font-heading-bold-20">Notifikasi</h4>
-                    <div className="d-flex gap-5 mx-3">
-                        <div className="bg-beranda w-75">
-                            <a variant="link" href="/" className="text-beranda">
-                                <p className="font-title-bold-16 text-start">Beranda</p>
-                            </a>
+                    <div className="d-flex gap-3 mx-3 mt-4 align-items-center">
+                        <div className="bg-beranda w-75" style={{ padding: '10px 5px' }}>
+                            <div className="d-flex">
+                                <a variant="link" href="/">
+                                    <Image src={icons.arrowLeft} alt="arrow-left" className="px-3" />
+                                </a>
+                                <a variant="link" href="/" className="text-beranda">
+                                    <p className="font-title-bold-16 text-start mb-0">Beranda</p>
+                                </a>
+                            </div>
                         </div>
-                        <p>filter</p>
-                        <p>cari</p>
+                        <a variant="link" href="/filter">
+                            <Image src={icons.filterButton} alt="filter" />
+                        </a>
+                        <a variant="link" href="/search">
+                            <Image src={icons.searchButton} alt="search" />
+                        </a>
                     </div>
                 </Container>
+
             </div>
             <Container className="py-5">
-                <Card className="mb-2 mx-3">
+                <Card className="mb-2 w-75">
                     <Card.Body>
                         <Row>
                             <Col md={10}>
-                                <Card.Title className="font-body-regular-14 text-muted">Promosi</Card.Title>
-                                <Card.Text className="font-title-regular-16">Dapatkan Potongan 50% Tiket!<br />
-                                    <span className="font-body-regular-14 text-muted">Syarat dan Ketentuan berlaku!</span>
-                                </Card.Text>
+                                <div className="d-flex align-items-start">
+                                    <Image src={icons.notifButton} alt="notif" className="me-3" />
+                                    <div>
+                                        <Card.Title className="font-body-regular-14 text-muted">Promosi</Card.Title>
+                                        <Card.Text className="font-title-regular-16">
+                                            Dapatkan Potongan 50% Tiket!<br />
+                                            <span className="font-body-regular-14 text-muted">Syarat dan Ketentuan berlaku!</span>
+                                        </Card.Text>
+                                    </div>
+                                </div>
                             </Col>
                             <Col md={2} className="text-end">
                                 <small className="font-body-regular-14 text-muted">20 Maret, 14:04</small>
@@ -38,10 +55,15 @@ const NotificationPage = () => {
                     <Card.Body>
                         <Row>
                             <Col md={10}>
-                                <Card.Title className="font-body-regular-14 text-muted">Notifikasi</Card.Title>
-                                <Card.Text className="font-title-regular-16">
-                                    Terdapat perubahan pada jadwal penerbangan kode booking 45GT6. Cek jadwal perjalanan Anda disini!
-                                </Card.Text>
+                                <div className="d-flex align-items-start">
+                                    <Image src={icons.notifButton} alt="notif" className="me-3" />
+                                    <div>
+                                        <Card.Title className="font-body-regular-14 text-muted">Notifikasi</Card.Title>
+                                        <Card.Text className="font-title-regular-16">
+                                            Terdapat perubahan pada jadwal penerbangan kode booking 45GT6. Cek jadwal perjalanan Anda disini!
+                                        </Card.Text>
+                                    </div>
+                                </div>
                             </Col>
                             <Col md={2} className="text-end">
                                 <small className="font-body-regular-14 text-muted">5 Maret, 14:04</small>
