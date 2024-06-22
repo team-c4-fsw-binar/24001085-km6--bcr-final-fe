2895e1904602679d78aae83ebcac23d5ed28d6fc
aisyahwlandr <aisyahwlandr@gmail.com>
2024-06-22 14:07:36 +0700

    try to fetch

--- src/redux/actions/checkout.js
+++ src/redux/actions/checkout.js
@@ -1,26 +1,46 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const postBooking = (bookingData) => async (dispatch, getState) => {
-    const { token } = getState().auth;
-    try {
-        const response = await axios.post(
-            `${import.meta.env.VITE_BACKEND_API}/api/bookings`,
-            bookingData,
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                },
-            }
-        );
+export const postBooking = (bookingData) => {
+    let data = new FormData();
 
-        if (response.status === 200 || response.status === 201) {
-            toast.success("Booking successful!");
-        } else {
+    data.append("departure_flight_id", bookingData.departure_flight_id);
+    data.append("return_flight_id", bookingData.return_flight_id);
+    data.append("price_amount", bookingData.price_amount);
+    data.append("seat_class", bookingData.seat_class);
+    data.append("adultCount", bookingData.adultCount);
+    data.append("childCount", bookingData.childCount);
+    data.append("babyCount", bookingData.babyCount);
+
+    bookingData.seats_id.forEach((seat, index) => {
+        data.append(`seats_id[${index}]`, seat);
+    });
+
+    bookingData.passengers.forEach((passenger, index) => {
+        data.append(`passengers[${index}][title]`, passenger.title);
+        data.append(`passengers[${index}][name]`, passenger.name);
+        data.append(`passengers[${index}][born_date]`, passenger.born_date);
+        data.append(`passengers[${index}][citizenship]`, passenger.citizenship);
+        data.append(`passengers[${index}][identity_number]`, passenger.identity_number);
+        data.append(`passengers[${index}][publisher_country]`, passenger.publisher_country);
+        data.append(`passengers[${index}][identity_expire_date]`, passenger.identity_expire_date);
+    });
+
+    let config = {
+        method: "post",
+        url: `${import.meta.env.VITE_BACKEND_API}/api/bookings`,
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            if (response.status === 200 || response.status === 201) {
+                toast.success("Booking successful!");
+            } else {
+                toast.error("Booking failed!");
+            }
+        })
+        .catch((error) => {
             toast.error("Booking failed!");
-        }
-    } catch (error) {
-        toast.error("Booking failed!");
-    }
+        });
 };
