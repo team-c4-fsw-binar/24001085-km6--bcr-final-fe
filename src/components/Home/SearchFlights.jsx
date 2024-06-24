367227fd1428e609e55b1d09691f36259f6fa8c4
dvlboo <kuhhhckr@gmail.com>
2024-06-24 21:30:27 +0700

    add components search flights

---
+++ src/components/Home/SearchFlights.jsx
@@ -0,0 +1,21 @@
+import { Button, Form, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const SearchFlightsComponents = () => {
+  return (
+    <Form>
+      
+      <Row>
+
+      </Row>
+
+      <Row>
+
+      </Row>
+
+      <Button as={Link} to="/search" type="submit">Cari Penerbangan</Button>
+    </Form>
+  )
+}
+
+export default SearchFlightsComponents;
