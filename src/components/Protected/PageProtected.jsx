a257742ae1c0612246a7af287fa363831b3e6a7a
dvlboo <kuhhhckr@gmail.com>
2024-06-30 03:33:38 +0700

    fixed : protected page search and checkout

--- src/components/Protected/PageProtected.jsx
+++ src/components/Protected/PageProtected.jsx
@@ -1,19 +1,27 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
+import PropTypes from "prop-types"
 
-const PageProtected = ({ childern }) => {
-  const navigate = useNavigate()
-
+const PageProtected = ({ children }) => {
+  const navigate = useNavigate();
   const homeData = useSelector((state) => state.flights.homeData)
 
   useEffect(() => {
     if (!homeData) {
       navigate("/")
     }
-  },[])
+  }, [homeData, navigate]);
 
-  return childern
+  if (!homeData) {
+    return null
+  }
+
+  return children;
 }
 
-export default PageProtected
+PageProtected.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default PageProtected;
